2af73437ea567dcf9ed3872f84e5247831c660ca
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2021-12-31 17:21:01 -0500

    added prototypes to a .h type

---
+++ MyVector.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template <class T>
+class MyVector{
+public:
+  MyVector();
+  int size();
+  int capacity();
+  bool empty() const;
+  void push_back(T n);
+  void pop_back(T n);
+  void pop_back();
+  void clear();
+  T &operator[](int n);
+private:
+  int myvector_size;
+  int myvector_cap;
+  T * data;
+};
+
+#include "MyVector.cxx"
